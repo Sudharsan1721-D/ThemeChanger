199692cd4a895995ed2910c0cf348db44e1c3eb6
Sudharsan1721-D <sudharsadhamodharan9361@gmail.com>
2025-06-25 11:04:22 +0530

    changes

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const light_btn = document.getElementById('light');
+const dark_btn = document.getElementById('dark');
+
+function light(){
+    document.body.style.backgroundColor = "lightblue";
+}
+
+function dark(){
+    document.body.style.backgroundColor = "red";
+}
